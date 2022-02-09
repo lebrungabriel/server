7a417b72f86eb1acbd90946bbcbed42816500be0
Gabriel Lebrun <lebr1.gabriel@gmail.com>
2022-02-09 09:44:43 +0100

    second test for deploy

--- index.js
+++ index.js
@@ -13,9 +13,11 @@ app.use(bodyParser.json({ limit: "30mb", extended: true }))
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }))
 app.use(cors())
 
-// app.use('/members', memberRoutes)
+app.use('/members', memberRoutes)
 
-app.get('/', memberRoutes)
+app.get('/', (req, res) => {
+    res.send('Hello to my app')
+})
 
 const PORT = process.env.PORT || 5000
 
