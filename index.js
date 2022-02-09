72ed8c28799df5b285ac3f4899561a72c73a0878
Gabriel Lebrun <lebr1.gabriel@gmail.com>
2022-02-09 09:38:17 +0100

    Debug home page

--- index.js
+++ index.js
@@ -13,14 +13,14 @@ app.use(bodyParser.json({ limit: "30mb", extended: true }))
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }))
 app.use(cors())
 
-app.use('/members', memberRoutes)
+// app.use('/members', memberRoutes)
 
-app.get('/', (req, res) => {
-    res.send('Hello to the Argonautes team')
-})
+app.get('/members', memberRoutes)
 
 const PORT = process.env.PORT || 5000
 
 mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => app.listen(PORT, () => console.log(`Server running on port : ${PORT}`)))
     .catch((error) => console.log(error.message))
+
+mongoose.set('useFindAndModify', false)
